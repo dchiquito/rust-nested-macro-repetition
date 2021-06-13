7e35207e5bedfa2f44042cda0d42293f3aa9a7a7
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-06-13 19:10:03 -0400

    Move call_them_all into it's own file

---
+++ src/call_them_all.rs
@@ -0,0 +1,36 @@
+fn f() {
+  println!("f");
+}
+
+fn g() {
+  println!("g");
+}
+
+fn foo(a: u32, b: u32, c: u32) {
+  println!("foo {} {} {}", a, b, c);
+}
+
+fn bar(a: u32, b: u32, c: u32) {
+  println!("bar {} {} {}", a, b, c);
+}
+
+macro_rules! call_them_all {
+  ($func:ident, $($funcs:ident),* : $($arg:expr),*) => {
+      call_them_all!($func: $($arg),*);
+      call_them_all!($($funcs),*: $($arg),*);
+  };
+  ($func:ident : $($arg:expr),*) => {
+      $func($($arg),*);
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  pub fn test_call_them_all() {
+    call_them_all!(f,g : );
+    call_them_all!(foo,bar : 1, 2, 3);
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-fn f() {
-    println!("f");
-}
+mod call_them_all;
 
-fn g() {
-    println!("g");
-}
-
-fn foo(a: u32, b: u32, c: u32) {
-    println!("foo {} {} {}", a, b, c);
-}
-
-fn bar(a: u32, b: u32, c: u32) {
-    println!("bar {} {} {}", a, b, c);
-}
-
-macro_rules! call_them_all {
-    ($func:ident, $($funcs:ident),* : $($arg:expr),*) => {
-        call_them_all!($func: $($arg),*);
-        call_them_all!($($funcs),*: $($arg),*);
-    };
-    ($func:ident : $($arg:expr),*) => {
-        $func($($arg),*);
-    }
-}
-
-fn main() {
-    call_them_all!(f,g : );
-    call_them_all!(foo,bar : 1, 2, 3);
-}
+fn main() {}
