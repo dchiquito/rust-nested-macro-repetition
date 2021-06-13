c26741767084614d76904346918c770701e2d842
Daniel Chiquito <daniel.chiquito@kitware.com>
2021-06-13 10:52:17 -0400

    The problem
    This is the macro I would like to write. Sadly, it does not work.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+fn f() {
+    println!("f");
+}
+
+fn g() {
+    println!("g");
+}
+
+fn foo(a: u32, b: u32, c: u32) {
+    println!("foo {} {} {}", a, b, c);
+}
+
+fn bar(a: u32, b: u32, c: u32) {
+    println!("bar {} {} {}", a, b, c);
+}
+
+macro_rules! call_them_all {
+    ($($func:ident),+ : $($arg:expr),*) => {
+        $(
+            // This nested repetition is not recognized?
+            $func($($arg)*);
+        )*
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    call_them_all!(f,g : );
+    call_them_all!(foo,bar : 1, 2, 3);
 }
